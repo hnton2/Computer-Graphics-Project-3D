5f08b7ff3244ad97205a476f80a8f105ab528069
hongocton0406 <59790401+hongocton0406@users.noreply.github.com>
2021-07-05 15:40:13 +0700

    animation for 3d models

--- js/main.js
+++ js/main.js
@@ -839,7 +839,6 @@ function ClearGeometry() {
 }
 function removeObject3D() {
     if(model3D.name !== '') {
-        console.log('Hello World')
         var selectedObject = scene.getObjectByName(model3D.name);
         scene.remove( selectedObject );
         scene.add(gridhelp);
@@ -943,23 +942,30 @@ function render() {
     renderer.render( scene, currentCamera );
 }
 function Rolling_animate() {
-    obj.rotation.x += 0.01;
-    obj.rotation.y += 0.02;
-
+    if(model3D.name != '') {
+        model3D.rotation.x += 0.01;
+        model3D.rotation.y += 0.02;
+    } else {
+        obj.rotation.x += 0.01;
+        obj.rotation.y += 0.02;
+    }
     animate = requestAnimationFrame(Rolling_animate);
     renderer.render(scene, currentCamera);
 }
 
 function Moving_animate() {
-    if(obj.position.y <= 10) {
-        obj.position.y -= 0.01;
-        obj.rotation.x -= 0.01;
-        obj.rotation.y -= 0.02;
-    }
-    else {
-        obj.position.y += 0.01;
-        obj.rotation.x += 0.01;
-        obj.rotation.y += 0.02;
+    if(model3D.name != '') {
+        model3D.position.x += 0.5
+    } else {
+        if(obj.position.y <= 10) {
+            obj.position.y -= 0.01;
+            obj.rotation.x -= 0.01;
+            obj.rotation.y -= 0.02;
+        } else {
+            obj.position.y += 0.01;
+            obj.rotation.x += 0.01;
+            obj.rotation.y += 0.02;
+        }
     }
     animate = requestAnimationFrame(Moving_animate);
     renderer.render(scene, currentCamera);
